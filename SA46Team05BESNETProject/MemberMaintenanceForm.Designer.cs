7f8c871c47e923262c1509c16d2b25395cb567a8
skxsephiroth <skxsephiroth87@gmail.com>
2018-03-23 09:50:17 +0800

    initial commit

---
+++ SA46Team05BESNETProject/MemberMaintenanceForm.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace SA46Team05BESNETProject
+{
+    partial class MemberMaintenanceForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MemberNameLabel = new System.Windows.Forms.Label();
+            this.FINLabel = new System.Windows.Forms.Label();
+            this.PhoneNumberLabel = new System.Windows.Forms.Label();
+            this.GenderLabel = new System.Windows.Forms.Label();
+            this.AgeLabel = new System.Windows.Forms.Label();
+            this.MemberNameTextBox = new System.Windows.Forms.TextBox();
+            this.NRICTextBox = new System.Windows.Forms.TextBox();
+            this.PhoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.GenderComboBox = new System.Windows.Forms.ComboBox();
+            this.PreviousButton = new System.Windows.Forms.Button();
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.InsertButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.NextButton = new System.Windows.Forms.Button();
+            this.FirstButton = new System.Windows.Forms.Button();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.LastButton = new System.Windows.Forms.Button();
+            this.SearchTextBox = new System.Windows.Forms.TextBox();
+            this.SearchGroupBox = new System.Windows.Forms.GroupBox();
+            this.SearchGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MemberNameLabel
+            // 
+            this.MemberNameLabel.AutoSize = true;
+            this.MemberNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MemberNameLabel.Location = new System.Drawing.Point(110, 37);
+            this.MemberNameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.MemberNameLabel.Name = "MemberNameLabel";
+            this.MemberNameLabel.Size = new System.Drawing.Size(76, 13);
+            this.MemberNameLabel.TabIndex = 0;
+            this.MemberNameLabel.Text = "Member Name";
+            // 
+            // FINLabel
+            // 
+            this.FINLabel.AutoSize = true;
+            this.FINLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FINLabel.Location = new System.Drawing.Point(110, 89);
+            this.FINLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.FINLabel.Name = "FINLabel";
+            this.FINLabel.Size = new System.Drawing.Size(55, 13);
+            this.FINLabel.TabIndex = 1;
+            this.FINLabel.Text = "NRIC/FIN";
+            // 
+            // PhoneNumberLabel
+            // 
+            this.PhoneNumberLabel.AutoSize = true;
+            this.PhoneNumberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PhoneNumberLabel.Location = new System.Drawing.Point(108, 248);
+            this.PhoneNumberLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.PhoneNumberLabel.Name = "PhoneNumberLabel";
+            this.PhoneNumberLabel.Size = new System.Drawing.Size(78, 13);
+            this.PhoneNumberLabel.TabIndex = 2;
+            this.PhoneNumberLabel.Text = "Phone Number";
+            // 
+            // GenderLabel
+            // 
+            this.GenderLabel.AutoSize = true;
+            this.GenderLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GenderLabel.Location = new System.Drawing.Point(110, 148);
+            this.GenderLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.GenderLabel.Name = "GenderLabel";
+            this.GenderLabel.Size = new System.Drawing.Size(42, 13);
+            this.GenderLabel.TabIndex = 3;
+            this.GenderLabel.Text = "Gender";
+            // 
+            // AgeLabel
+            // 
+            this.AgeLabel.AutoSize = true;
+            this.AgeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AgeLabel.Location = new System.Drawing.Point(110, 195);
+            this.AgeLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.AgeLabel.Name = "AgeLabel";
+            this.AgeLabel.Size = new System.Drawing.Size(26, 13);
+            this.AgeLabel.TabIndex = 4;
+            this.AgeLabel.Text = "Age";
+            // 
+            // MemberNameTextBox
+            // 
+            this.MemberNameTextBox.Location = new System.Drawing.Point(256, 37);
+            this.MemberNameTextBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.MemberNameTextBox.Name = "MemberNameTextBox";
+            this.MemberNameTextBox.Size = new System.Drawing.Size(212, 20);
+            this.MemberNameTextBox.TabIndex = 5;
+            // 
+            // NRICTextBox
+            // 
+            this.NRICTextBox.Location = new System.Drawing.Point(256, 89);
+            this.NRICTextBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.NRICTextBox.Name = "NRICTextBox";
+            this.NRICTextBox.Size = new System.Drawing.Size(182, 20);
+            this.NRICTextBox.TabIndex = 6;
+            // 
+            // PhoneNumberTextBox
+            // 
+            this.PhoneNumberTextBox.Location = new System.Drawing.Point(254, 245);
+            this.PhoneNumberTextBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.PhoneNumberTextBox.Name = "PhoneNumberTextBox";
+            this.PhoneNumberTextBox.Size = new System.Drawing.Size(182, 20);
+            this.PhoneNumberTextBox.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(256, 192);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(101, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // GenderComboBox
+            // 
+            this.GenderComboBox.FormattingEnabled = true;
+            this.GenderComboBox.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.GenderComboBox.Location = new System.Drawing.Point(256, 145);
+            this.GenderComboBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.GenderComboBox.Name = "GenderComboBox";
+            this.GenderComboBox.Size = new System.Drawing.Size(121, 21);
+            this.GenderComboBox.TabIndex = 10;
+            // 
+            // PreviousButton
+            // 
+            this.PreviousButton.Location = new System.Drawing.Point(112, 313);
+            this.PreviousButton.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.PreviousButton.Name = "PreviousButton";
+            this.PreviousButton.Size = new System.Drawing.Size(47, 35);
+            this.PreviousButton.TabIndex = 11;
+            this.PreviousButton.Text = "<";
+            this.PreviousButton.UseVisualStyleBackColor = true;
+            // 
+            // UpdateButton
+            // 
+            this.UpdateButton.Location = new System.Drawing.Point(232, 313);
+            this.UpdateButton.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(79, 35);
+            this.UpdateButton.TabIndex = 12;
+            this.UpdateButton.Text = "Update";
+            this.UpdateButton.UseVisualStyleBackColor = true;
+            // 
+            // InsertButton
+            // 
+            this.InsertButton.Location = new System.Drawing.Point(360, 313);
+            this.InsertButton.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.InsertButton.Name = "InsertButton";
+            this.InsertButton.Size = new System.Drawing.Size(77, 35);
+            this.InsertButton.TabIndex = 13;
+            this.InsertButton.Text = "Insert";
+            this.InsertButton.UseVisualStyleBackColor = true;
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(497, 313);
+            this.DeleteButton.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(74, 35);
+            this.DeleteButton.TabIndex = 14;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            // 
+            // NextButton
+            // 
+            this.NextButton.Location = new System.Drawing.Point(644, 313);
+            this.NextButton.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(47, 35);
+            this.NextButton.TabIndex = 15;
+            this.NextButton.Text = ">";
+            this.NextButton.UseVisualStyleBackColor = true;
+            // 
+            // FirstButton
+            // 
+            this.FirstButton.Location = new System.Drawing.Point(112, 375);
+            this.FirstButton.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.FirstButton.Name = "FirstButton";
+            this.FirstButton.Size = new System.Drawing.Size(47, 36);
+            this.FirstButton.TabIndex = 16;
+            this.FirstButton.Text = "<<";
+            this.FirstButton.UseVisualStyleBackColor = true;
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SearchButton.Location = new System.Drawing.Point(263, 25);
+            this.SearchButton.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(74, 36);
+            this.SearchButton.TabIndex = 17;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            // 
+            // LastButton
+            // 
+            this.LastButton.Location = new System.Drawing.Point(644, 375);
+            this.LastButton.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.LastButton.Name = "LastButton";
+            this.LastButton.Size = new System.Drawing.Size(47, 36);
+            this.LastButton.TabIndex = 18;
+            this.LastButton.Text = ">>";
+            this.LastButton.UseVisualStyleBackColor = true;
+            // 
+            // SearchTextBox
+            // 
+            this.SearchTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SearchTextBox.Location = new System.Drawing.Point(36, 29);
+            this.SearchTextBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.SearchTextBox.Name = "SearchTextBox";
+            this.SearchTextBox.Size = new System.Drawing.Size(182, 20);
+            this.SearchTextBox.TabIndex = 19;
+            // 
+            // SearchGroupBox
+            // 
+            this.SearchGroupBox.Controls.Add(this.SearchTextBox);
+            this.SearchGroupBox.Controls.Add(this.SearchButton);
+            this.SearchGroupBox.Font = new System.Drawing.Font("Segoe Print", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SearchGroupBox.Location = new System.Drawing.Point(232, 355);
+            this.SearchGroupBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.SearchGroupBox.Name = "SearchGroupBox";
+            this.SearchGroupBox.Padding = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.SearchGroupBox.Size = new System.Drawing.Size(361, 78);
+            this.SearchGroupBox.TabIndex = 20;
+            this.SearchGroupBox.TabStop = false;
+            this.SearchGroupBox.Text = "Search by NRIC/FIN";
+            // 
+            // MemberMaintenanceForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 449);
+            this.Controls.Add(this.SearchGroupBox);
+            this.Controls.Add(this.LastButton);
+            this.Controls.Add(this.FirstButton);
+            this.Controls.Add(this.NextButton);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.InsertButton);
+            this.Controls.Add(this.UpdateButton);
+            this.Controls.Add(this.PreviousButton);
+            this.Controls.Add(this.GenderComboBox);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.PhoneNumberTextBox);
+            this.Controls.Add(this.NRICTextBox);
+            this.Controls.Add(this.MemberNameTextBox);
+            this.Controls.Add(this.AgeLabel);
+            this.Controls.Add(this.GenderLabel);
+            this.Controls.Add(this.PhoneNumberLabel);
+            this.Controls.Add(this.FINLabel);
+            this.Controls.Add(this.MemberNameLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "MemberMaintenanceForm";
+            this.Text = "Member Maintenance Form";
+            this.SearchGroupBox.ResumeLayout(false);
+            this.SearchGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label MemberNameLabel;
+        private System.Windows.Forms.Label FINLabel;
+        private System.Windows.Forms.Label PhoneNumberLabel;
+        private System.Windows.Forms.Label GenderLabel;
+        private System.Windows.Forms.Label AgeLabel;
+        private System.Windows.Forms.TextBox MemberNameTextBox;
+        private System.Windows.Forms.TextBox NRICTextBox;
+        private System.Windows.Forms.TextBox PhoneNumberTextBox;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.ComboBox GenderComboBox;
+        private System.Windows.Forms.Button PreviousButton;
+        private System.Windows.Forms.Button UpdateButton;
+        private System.Windows.Forms.Button InsertButton;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Button NextButton;
+        private System.Windows.Forms.Button FirstButton;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Button LastButton;
+        private System.Windows.Forms.TextBox SearchTextBox;
+        private System.Windows.Forms.GroupBox SearchGroupBox;
+    }
+}
+
--- SA46Team05BESNETProject/TemplateForm.cs
+++ SA46Team05BESNETProject/TemplateForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SA46Team05BESNETProject
 {
-    public partial class Form1 : Form
+    public partial class TemplateForm : Form
     {
-        public Form1()
+        public TemplateForm()
         {
             InitializeComponent();
         }
